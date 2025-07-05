383213c6a46b88b9971270927edab1190d6801a3
Ramon Madeira Tomaz <ramonmadeira5@gmail.com>
2025-07-05 16:44:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-
-document.getElementById("setaDir").addEventListener("click", function(){   
-if (document.getElementById("audEstoque").style.display === "block") {
-    document.getElementById("audEstoque").style.display = "none";
-    document.getElementById("audPedidos").style.display = "block";
-} else {
-    document.getElementById("audEstoque").style.display = "block";
-    document.getElementById("audPedidos").style.display = "none";
-}
+document.getElementById("seta-esquerda").addEventListener("click", function() {
+    if (document.querySelector(".audestoque").style.display === "none") {
+        document.querySelector(".audestoque").style.display = "flex";
+        document.querySelector(".audpedido").style.display = "none";
+    }
+    else {
+        document.querySelector(".audestoque").style.display = "none";
+        document.querySelector(".audpedido").style.display = "flex";
+    }
 });
 
-document.getElementById("setaEsq").addEventListener("click", function(){  
-  if (document.getElementById("audEstoque").style.display === "block") {
-    document.getElementById("audEstoque").style.display = "none";
-    document.getElementById("audPedidos").style.display = "block";
-  } else {
-    document.getElementById("audEstoque").style.display = "block";
-    document.getElementById("audPedidos").style.display = "none";
+document.getElementById("seta-direita").addEventListener("click", function() {
+  if (document.querySelector(".audpedido").style.display === "none") {
+      document.querySelector(".audpedido").style.display = "flex";
+      document.querySelector(".audestoque").style.display = "none";
+  }
+  else {
+      document.querySelector(".audpedido").style.display = "none";
+      document.querySelector(".audestoque").style.display = "flex";
   }
 });
